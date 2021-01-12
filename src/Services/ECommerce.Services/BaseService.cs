f4e419c4efbb32f6d27a1e5c3cdb9fddd1a2b3e4
Павел <pav.savov@gmail.com>
2021-01-12 22:49:19 +0200

    Generic BaseService class created

---
+++ src/Services/ECommerce.Services/BaseService.cs
@@ -0,0 +1,43 @@
+﻿using ECommerce.DataAccess;
+using ECommerce.Models.BaseModel;
+using ECommerce.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ECommerce.Services
+{
+    public abstract class BaseService<TEntity> : ICRUDService<TEntity>
+        where TEntity : BaseDbModel
+    {
+        private readonly ECommerceDbContext _context;
+        public BaseService(ECommerceDbContext context)
+        {
+            _context = context;
+        }
+        public virtual Task<TEntity> Create(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<IEnumerable<TEntity>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<TEntity> GetById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual Task<TEntity> Update(TEntity Entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
